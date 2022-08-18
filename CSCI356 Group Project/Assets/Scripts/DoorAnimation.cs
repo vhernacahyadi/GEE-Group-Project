03c9e83cccac8ab89f4a67bcdaffce3ecc33d8ec
YS <yungsoon9807@gmail.com>
2022-08-18 21:31:16 +0800

    new scene 1 with door

---
+++ CSCI356 Group Project/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    public float SPEED = 1.0F;  // rotation speed
+    private int stopAngle;
+    private Vector3 direction;
+    private bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+        stopAngle = (int)transform.eulerAngles.y; // stay in the current position
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((int)transform.eulerAngles.y != stopAngle) // rotate untill current angle != stop angle
+        {
+            transform.Rotate(direction * SPEED);
+        }
+
+        if( Input.GetKeyDown(KeyCode.O)  ) 
+        {
+            stopAngle = 90;              // stop abgle 90
+            direction = Vector3.up;  // direction around Y clock-wise
+            // Debug.Log("O key pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            stopAngle = 0;                // stop abgle 90
+            direction = -Vector3.up;  // direction around Y counter clockwise
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            stopAngle = isOpen?0:90;                // stop abgle 90
+            direction = isOpen ? -Vector3.up:Vector3.up;  // direction around Y counter clockwise
+            isOpen = !isOpen;
+        }
+    }
+}
