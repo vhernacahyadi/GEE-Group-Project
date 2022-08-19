9bb05d2df9ccea5a98042f0f0de63ce814c73000
kdjudyan001 <57139664+kdjudyan001@users.noreply.github.com>
2022-08-19 22:32:14 +0800

    Rename script to SlimeController

--- CSCI356 Group Project/Assets/Scripts/SlimeController.cs
+++ CSCI356 Group Project/Assets/Scripts/SlimeController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeAI : MonoBehaviour
+public class SlimeController : MonoBehaviour
 {
     [SerializeField]
     private float health;
