6483218146893fdf0d19127db3904f4d09cdbf7d
YS <yungsoon9807@gmail.com>
2022-08-21 00:52:20 +0800

    add pickup ammo

--- CSCI356 Group Project/Assets/Scripts/AmmoItem.cs
+++ CSCI356 Group Project/Assets/Scripts/AmmoItem.cs
@@ -1,35 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AmmoItem : MonoBehaviour
 {
+    private GameObject player;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag.Equals("Player"))
-        {
-            Debug.Log("Player near ammo");
-
-            if (TryGetComponent(out gunShooter gunShooter))
+        Vector3 direction = player.transform.position - this.transform.position;
+        direction.y = 0;
+        if (Input.GetKeyDown(KeyCode.Q))
             {
-                Debug.Log(gunShooter.bulletAmount);
+            Debug.Log(direction.magnitude);
+            if (direction.magnitude < 1)
+            {
+                Debug.Log("Player near ammo");
+                Debug.Log(GameManager.bulletAmount);
 
-                gunShooter.bulletAmount += 6;
-                Debug.Log("gunShooter.bulletAmount after" + gunShooter.bulletAmount);
-                Destroy(gameObject);
+                 GameManager.bulletAmount += 6;
+                 Debug.Log("gunShooter.bullet amount left" + GameManager.bulletAmount);
+                  Destroy(gameObject);
+
             }
         }
     }
+    //private void OnCollisionEnter(Collision cls)
+    //{
+    //    Debug.Log("OnCollisionEnter");
+    //    if (cls.gameObject.tag.Equals("Player"))
+    //    {
+    //        Debug.Log("Player OnCollisionEnter");
+
+    //        if (TryGetComponent(out gunShooter gunShooter))
+    //        {
+    //            Debug.Log(gunShooter.bulletAmount);
+
+    //            gunShooter.bulletAmount += 6;
+    //            Debug.Log("gunShooter.bulletAmount after" + gunShooter.bulletAmount);
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
 
 }
