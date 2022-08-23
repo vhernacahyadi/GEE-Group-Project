8a169ec140621286dcefae2aeb3b999fcce4bc09
YS <yungsoon9807@gmail.com>
2022-08-23 16:40:48 +0800

    update platform

--- CSCI356 Group Project/Assets/Scripts/MovingPlatformScene3.cs
+++ CSCI356 Group Project/Assets/Scripts/MovingPlatformScene3.cs
@@ -6,7 +6,7 @@ public class MovingPlatformScene3 : MonoBehaviour
 {
     [SerializeField] GameObject[] wayPoints;
     int currentWayPointIndex = 0;
-     float speed = 5f;
+     float speed = 3.5f;
 
     // Update is called once per frame
     void Update()
