47cede13960a0690dd5af3d3977094c9b202171d
YS <yungsoon9807@gmail.com>
2022-08-24 00:23:34 +0800

    update

--- CSCI356 Group Project/Assets/Bounce.cs
+++ CSCI356 Group Project/Assets/Bounce.cs
@@ -10,28 +10,38 @@ public class Bounce : MonoBehaviour
     public float bounceHelight=500;
     //[SerializeField]private Animator playerAnimator;
     private bool onAir = false;
+    GameObject rbBounce;
+    Vector3 originalPosition;
+    Vector3 desiredPosition;
     //[SerializeField public Transform endMarker;
     private void Start()
     {
-        //PlayerBall = GameObject.FindWithTag("TagOfBallObject").GetComponent<Rigidbody>();
+        rbBounce = GameObject.FindWithTag("Bounce");
+        Debug.Log("going bounce");
+        originalPosition = rbBounce.transform.position;
+        desiredPosition = new Vector3(rbBounce.transform.position.x, rbBounce.transform.position.y + 10, rbBounce.transform.position.z);
     }
 
     //private void OnTriggerEnter(Collider other)
-            private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other)
     {
         GameObject bouncer = other.gameObject;
         Rigidbody rb = bouncer.GetComponent<Rigidbody>();
-        GameObject rbBounce = GameObject.FindWithTag("Bounce");
-        Debug.Log("going bounce");
-        Vector3 desiredPosition = new Vector3(rbBounce.transform.position.x, rbBounce.transform.position.y+ 5, rbBounce.transform.position.z);
-        if (other.gameObject.tag == "Player"&& onAir==false)
+
+        if (other.gameObject.tag == "Player")
         {
             Debug.Log("on bounce");
             rb.AddForce(Vector3.up * bounceHelight);
             // rbBounce.transform.position = new Vector3(rbBounce.position.x, 5, rbBounce.position.z);
+            if (onAir == false)
+            {
+                rbBounce.transform.position = Vector3.MoveTowards(originalPosition, desiredPosition, Time.deltaTime * 6f);
+                Debug.Log(originalPosition);
+                rbBounce.transform.position = Vector3.MoveTowards(desiredPosition, originalPosition, Time.deltaTime * 0.4f);
 
-            rbBounce.transform.position = Vector3.MoveTowards(rbBounce.transform.position, desiredPosition, Time.deltaTime * 3f);
-            //onAir = true;
+                onAir = true;
+            }
+
         }
     }
 }
