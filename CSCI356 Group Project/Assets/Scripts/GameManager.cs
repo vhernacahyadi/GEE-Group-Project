bcb4faf3d01fb41a36698e0ece8a739fdeac4d31
RX-Rexax <80836157+RX-Rexax@users.noreply.github.com>
2022-08-24 19:06:09 +0800

    Added Events to end the Level & Added GameOver Scene
    Added Events to end the Level
    Added GameOver Scene

--- CSCI356 Group Project/Assets/Scripts/GameManager.cs
+++ CSCI356 Group Project/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -16,8 +17,32 @@ public class GameManager : MonoBehaviour
         CompleteLevelUI.SetActive(true);
     }
 
+    // Open up CompleteLevel Room after EndGame
     public void EndGame()
     {
+        Debug.Log("Awake:" + SceneManager.GetActiveScene().name);
+        if(SceneManager.GetActiveScene().name == "Level 1")
+        {
+            GameObject cannon = GameObject.Find("Cannon"); // Finds a game object by name
+            cannon.transform.position = new Vector3(cannon.transform.position.x, 3.5f, cannon.transform.position.z);
+        }
+
+        if(SceneManager.GetActiveScene().name == "Level 2")
+        {
+            // activate button
+            GameObject sw = GameObject.Find("Switch"); // Finds a game object by name
+            sw.transform.position = new Vector3(sw.transform.position.x, 2.5f, sw.transform.position.z);
+        }
+
+        if (SceneManager.GetActiveScene().name == "Level 3")
+        {
+            // Save Name & Score here...
+
+
+            // Go to the next scene [Game Over Scene]
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
         Debug.Log("Game Over!");
     }
 
---
+++ CSCI356 Group Project/Assets/Scripts/UI Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    public Text FinalScore;
+
+    void Start()
+    {
+        FinalScore.text = EnterName.Score.ToString();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Save Scores Here...
+
+    }
+
+    public void GoToMainMenu()
+    {
+        // Go back to main menu
+        Debug.Log("Main Menu!");
+        SceneManager.LoadScene("Main Menu");
+    }
+}
