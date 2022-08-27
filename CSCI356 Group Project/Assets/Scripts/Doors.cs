3d3bbb95aaece10b2bc6803693b8eaecbb2cbd8a
RX-Rexax <80836157+RX-Rexax@users.noreply.github.com>
2022-08-27 17:32:03 +0800

    Added Door script

---
+++ CSCI356 Group Project/Assets/Scripts/Doors.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+    Animator doorAnim;
+    bool doorOpen;
+
+	// Use this for initialization
+	void Start () {
+        doorOpen = false;
+        doorAnim = GetComponent<Animator>();
+	}
+    
+    // an event function that is called when an object enters the trigger zone
+    void OnTriggerEnter(Collider col) 
+    {
+        if(col.gameObject.tag == "Player")  // check the tag
+        {
+            doorOpen = true;
+            doorAnim.SetTrigger("Open");
+           // Debug.Log("Player Entered Trigger Zone");      
+        }
+    }
+
+    // an event function that is called when an object leaves the trigger zone
+    private void OnTriggerExit(Collider col)
+    {
+        if(doorOpen==true)
+        {
+            doorOpen = false;
+            doorAnim.SetTrigger("Close");
+        }
+
+       // Debug.Log("Exit Triger Zone");
+    }
+
+}
