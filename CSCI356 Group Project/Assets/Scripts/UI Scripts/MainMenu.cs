b5ccd16db36288ae5b4aadfb80a1b7a264b08c6c
kdjudyan001 <57139664+kdjudyan001@users.noreply.github.com>
2022-08-28 21:34:26 +0800

    Store volume setting to PlayerPrefs

--- CSCI356 Group Project/Assets/Scripts/UI Scripts/MainMenu.cs
+++ CSCI356 Group Project/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        // Get audio volume from PlayerPrefs
+        AudioListener.volume = PlayerPrefs.HasKey("Volume") ? PlayerPrefs.GetFloat("Volume") : 1.0f;
+    }
 
     public void QuitGame()
     {
