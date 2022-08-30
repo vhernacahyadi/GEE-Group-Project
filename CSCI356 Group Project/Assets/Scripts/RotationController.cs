71b21180b05bd1a31be3aa3ce7ec78208b103fbc
kdjudyan001 <57139664+kdjudyan001@users.noreply.github.com>
2022-08-30 09:53:11 +0800

    Remove unused variables

--- CSCI356 Group Project/Assets/Scripts/RotationController.cs
+++ CSCI356 Group Project/Assets/Scripts/RotationController.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class RotationController : MonoBehaviour
 {
-    [SerializeField] private float HorizontalSens = 3.0f;
-    [SerializeField] private float VerticalSens = 2.0f;
-    private GameObject character;   //the class GameObject is G and O capital
+    //[SerializeField] private float HorizontalSens = 3.0f;
+    //[SerializeField] private float VerticalSens = 2.0f;
+    private GameObject character; 
 
     // Start is called before the first frame update
     void Start()
     {
         //disable mouse cursor
         Cursor.lockState = CursorLockMode.Locked;
-
-        //destroys object
-        //this.gameObject.Destroy();  //when accessing a game object's property, is small g and capital O
 
         //Assign a parent object
         character = this.transform.parent.gameObject;
@@ -24,12 +21,13 @@ public class RotationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //dont use var, not good programming practice in C#
         Vector2 mouseMove = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         //Rotate the character horizontally
         character.transform.Rotate(0, mouseMove.x, 0);
-        transform.Rotate(-mouseMove.y, 0, 0);   //Rotate the caamera vertically
+
+        //Rotate the caamera vertically
+        transform.Rotate(-mouseMove.y, 0, 0);   
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
