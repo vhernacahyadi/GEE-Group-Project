727041b9df0e59d98216363e7ea7577e17d3fda5
Vherna <vhernacahyadi4@gmail.com>
2022-08-30 12:22:23 +0800

    clamped the player camera's rotation

--- CSCI356 Group Project/Assets/Scripts/RotationController.cs
+++ CSCI356 Group Project/Assets/Scripts/RotationController.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class RotationController : MonoBehaviour
 {
-    //[SerializeField] private float HorizontalSens = 3.0f;
-    //[SerializeField] private float VerticalSens = 2.0f;
-    private GameObject character; 
+    private GameObject character;
+    private float verticalRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +20,20 @@ public class RotationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        verticalRotation = transform.rotation.eulerAngles.x;
         Vector2 mouseMove = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         //Rotate the character horizontally
         character.transform.Rotate(0, mouseMove.x, 0);
 
         //Rotate the caamera vertically
-        transform.Rotate(-mouseMove.y, 0, 0);   
+        Debug.Log(transform.rotation.eulerAngles);
+        if (transform.rotation.eulerAngles.x <= 40 || transform.rotation.eulerAngles.x >= 310)
+            transform.Rotate(-mouseMove.y, 0, 0);
+        else if(transform.rotation.eulerAngles.x > 40 && transform.rotation.eulerAngles.x < 100)
+            transform.rotation = Quaternion.Euler(39.9f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        else if (transform.rotation.eulerAngles.x > 100 && transform.rotation.eulerAngles.x < 310)
+            transform.rotation = Quaternion.Euler(310.1f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
